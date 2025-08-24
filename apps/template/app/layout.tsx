af2f9a9cad5f1285cfeca70a7d9912723acf72e8
Jason Kennemer <63683738+Jason-Kennemer@users.noreply.github.com>
2025-08-24 02:22:00 -0500

    revert app template css file

--- apps/template/app/layout.tsx
+++ apps/template/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import './globals.css';
+import '@valesco/ui/globals.css';
 import { Providers } from '@/components/providers';
 
 export const metadata: Metadata = {
